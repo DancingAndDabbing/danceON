e4a9cbd08322dc45e2aa4f17f98e8568377e2d49
Yoav Bergner <ybergner@gmail.com>
2020-06-23 12:35:15 -0400

    update example

--- editor.js
+++ editor.js
@@ -7,24 +7,24 @@ let startingCode = `(pose, poseHistory) => [
         what: 'circle',
         when: true,
         where: {
-            x: pose.leftWrist.x,
-            y: pose.leftWrist.y,
+            x: pose.leftHip.x,
+            y: pose.leftHip.y,
         },
         how: {
-            d: [30, 60, 80],
+            d: [30],
             fill: 'rgba(0,0,255, 0.25)',
             stroke: 50
         }
     },
     {
         what: 'circle',
-        when: pose.rightWrist.y < pose.rightHip.y,
+        when: pose.rightWrist.y < pose.nose.y,
         where: {
             start: {
                 x: pose.rightWrist.x,
                 y: pose.rightWrist.y,
-                velocityX: random(-12, -7),
-                velocityY: random(-0.6, 0.6)
+                velocityX: random(-5, 5),
+                velocityY: random(-5, 5)
             },
             accelerationX: 0,
             accelerationY: 0
@@ -32,6 +32,25 @@ let startingCode = `(pose, poseHistory) => [
         how: {
             d: [{frame: 0, value: 0}, {frame: 30, value: 30}],
             fill: 'rgba(255,0,0, 0.25)',
+            stroke: 50
+        }
+    },
+    {
+        what: 'circle',
+        when: pose.leftWrist.y < pose.nose.y,
+        where: {
+            start: {
+                x: pose.leftWrist.x,
+                y: pose.leftWrist.y,
+                velocityX: random(-5, 5),
+                velocityY: random(-5, 5)
+            },
+            accelerationX: 0,
+            accelerationY: 0
+        },
+        how: {
+            d: [{frame: 0, value: 0}, {frame: 30, value: 30}],
+            fill: 'rgba(0,255,0, 0.25)',
             stroke: 50
         }
     }
