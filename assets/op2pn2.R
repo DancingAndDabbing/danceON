00ca83e02c10dff9293aca237d894c5628828f3e
Yoav Bergner <ybergner@gmail.com>
2020-07-04 14:35:41 -0400

    R conversion script
    still not working, but cacheing

--- assets/op2pn2.R
+++ assets/op2pn2.R
@@ -1,6 +1,5 @@
 #!/usr/bin/env Rscript
 library(RJSONIO)
-
 args <- commandArgs(trailingOnly = TRUE)
 
 if (length(args)==0) {
@@ -20,33 +19,46 @@ coco2posenet <- structure(list(keypt0 = 0:18,
                           row.names = c(NA, -19L))
 excludeRows <- which(is.na(coco2posenet$posenet))
 
-# need to accommodate this ordering
-# ['nose', '0'],
-# ['leftEye', '16'], ['rightEye', '15'],
-# ['leftEar', '18'], ['rightEar', '17'],
-# ['leftShoulder', '5'], ['rightShoulder', '2'],
-# ['leftElbow', '6'], ['rightElbow', '3'],
-# ['leftWrist', '7'], ['rightWrist', '4'],
-# ['leftHip', '12'], ['rightHip', '9'],
-# ['leftKnee', '13'], ['rightKnee', '10'],
-# ['leftAnkle', '14'], ['rightAnkle', '11']
+
 
 newOrder <- c('nose', 'leftEye', 'rightEye',
-                      'leftEar', 'rightEar',
-                      'leftShoulder', 'rightShoulder',
-                      'leftElbow', 'rightElbow',
-                      'leftWrist', 'rightWrist',
-                      'leftHip', 'rightHip',
-                      'leftKnee', 'rightKnee',
-                      'leftAnkle','rightAnkle')
+              'leftEar', 'rightEar',
+              'leftShoulder', 'rightShoulder',
+              'leftElbow', 'rightElbow',
+              'leftWrist', 'rightWrist',
+              'leftHip', 'rightHip',
+              'leftKnee', 'rightKnee',
+              'leftAnkle','rightAnkle')
 
 
-convertCOCOdata2 <- function(jsonfile) {
-  frames <- fromJSON(jsonfile)
+
+
+## This is for multi-file output
+getFramesFromJSON <- function(output_dir) {
+  # frames will be an multi-dimensional lsit array of framenums x people x [COCO model]
+  frames <- list()
+  framenum <- 1
+  for (jfile in grep(".json",list.files(path=output_dir), value=T)){ #need to make sure to ignore non-JSON files
+    json_file <- fromJSON(file.path(output_dir,jfile)) #reading each json file, convert to R list
+    # it can also happen that there are no people in the frame
+    if (length(json_file$people)) {
+      # We will append the frames list with a matrix of pose keypoints
+      # First two columns are planar coordinates, thrid is confidence.
+      pose_keypoints2d <- lapply(json_file$people, function(a){
+        matrix(a$pose_keypoints_2d, ncol=3, byrow = TRUE) # DON'T throw away third column
+      })
+      frames[[framenum]] <- pose_keypoints2d # each frame gets a list of matrices
+      framenum <- framenum+1
+    }
+  }
+  return(frames)
+}
+
+convertCOCOmultiFile <- function(jsondir) {
+  frames <- getFramesFromJSON(jsondir)
   poseNetVersion <- vector("list",length(frames))
   for (j in 1:length(frames)) {
-    tmp <- matrix(frames[[j]]$keypoints, ncol=3, byrow = TRUE)
-    score <- frames[[j]]$score
+    tmp <- frames[[j]][[1]][1:19,]
     tmp <- data.frame(tmp, row.names=coco2posenet$cocopart[1:nrow(tmp)])
     names(tmp) <- c("x","y","conf")
     tmp$pnetname <- coco2posenet$posenet[1:nrow(tmp)]
@@ -54,7 +66,8 @@ convertCOCOdata2 <- function(jsonfile) {
     tmp <- tmp[-excludeRows,]
     # and finally reorder to match Willie's code
     rownames(tmp) <- tmp$pnetname
-
+    tmp <- tmp[newOrder,]
+    # tmp[tmp==0] <- "null"
     poseNetVersion[[j]] <- vector("list",1) # this is where 1-person is hard-coded
     poseNetVersion[[j]][[1]]$pose <- list()
     poseNetVersion[[j]][[1]]$pose$keypoints <- list()
@@ -71,9 +84,9 @@ convertCOCOdata2 <- function(jsonfile) {
   return(poseNetVersion)
 }
 
+####
 # read into R form openpose
-file <- args[1]
-poses2 <- convertCOCOdata2(file)
-
-str <- paste0('{"data":',toJSON(poses2, collapse = "\n"),'}')
-write(str, paste0(strsplit(file,".json")[[1]][1],"_converted3.json"))
+dir <- args[1]
+poses <- convertCOCOmultiFile(dir)
+str <- paste0('{"data":',toJSON(poses, collapse = ""),'}')
+write(str, file=paste0(dir,".json"))
