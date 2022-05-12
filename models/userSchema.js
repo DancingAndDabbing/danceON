bdc99008edae0000294f239a6680fb599c278da7
Naren Dhyani <31188643+dhyani15@users.noreply.github.com>
2022-05-12 12:29:26 -0400

    add method to userschema

--- models/userSchema.js
+++ models/userSchema.js
@@ -4,6 +4,10 @@ const userSchema = new mongoose.Schema({
     username: {type: String, required: true, unique: true},
     password: {type: String,},
 });
+userSchema.methods.validPassword = function( pwd ) {
+    // EXAMPLE CODE!
+    return ( this.password === pwd );
+};
 userSchema.plugin(passportLocalMongoose);
 // userSchema.plugin(encrypt, {secret:process.env.SECRET, encryptedFields: ['password']});
 const User = mongoose.model("User",userSchema);
