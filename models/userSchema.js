76091680842d31a5aa3c016005dc5ceaf1316abc
Naren Dhyani <31188643+dhyani15@users.noreply.github.com>
2022-05-11 23:41:02 -0400

    fixes

--- models/userSchema.js
+++ models/userSchema.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const passportLocalMongoose = require('passport-local-mongoose');
 const userSchema = new mongoose.Schema({
     username: {type: String, required: true, unique: true},
-    password: {type: String, required: true},
+    password: {type: String,},
 });
 userSchema.plugin(passportLocalMongoose);
 // userSchema.plugin(encrypt, {secret:process.env.SECRET, encryptedFields: ['password']});
