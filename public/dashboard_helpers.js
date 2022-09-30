d5f2d6ea5f00316c47de74633a3a5c8e024086d2
Naren Dhyani <31188643+dhyani15@users.noreply.github.com>
2022-09-30 06:49:42 -0400

    fix

--- public/dashboard_helpers.js
+++ public/dashboard_helpers.js
@@ -12,17 +12,9 @@ async function loadUsers() {
         const row = document.createElement("tr");
         row.setAttribute("data-id", user._id);
         row.innerHTML = `
-        <th>${i+1}</th>
-        <td>title="${user.username}">${user.username}</td>
+        <th>${i}</th>
+        <td>title="${user.username}</td>
         <td>${user.admin ? "Admin" : "Contributor"}</td>
-        <td>23</td>
-        <td>12</td>
-        <td>3</td>
-        <td>68</td>
-        <td>36</td>
-        <td>+32</td>
-        <td>81</td>
-        <td>Qualification for the <a href="https://en.wikipedia.org/wiki/2016%E2%80%9317_UEFA_Champions_League#Group_stage" title="2016–17 UEFA Champions League">Champions League group stage</a></td>
         `;
         table.appendChild(row);
     }
