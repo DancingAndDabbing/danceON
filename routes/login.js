a362e4958c9afee82a1dbfaaa0a009990d23f71d
Naren Dhyani <31188643+dhyani15@users.noreply.github.com>
2022-09-30 05:43:16 -0400

    bugfix

--- routes/login.js
+++ routes/login.js
@@ -4,24 +4,29 @@ const path = require('path');
 const User = require('../models/userSchema');
 const passport = require('passport');
 
-router.post('/', function(req, res) {
+router.post('/',passport.authenticate('local'),function(req, res) {
   console.log(req.body.username)
-  User.findOne({username: req.body.username}, function(err, user){
-    console.log("The user is: "+user.admin);
-    if (user.admin === "true"){
-      // res.redirect('/admin');
-      // admin = true;
-      passport.authenticate('local',{
-        successRedirect: '/admin',
-        failureRedirect: '/'
-      })
-    } else{
-      passport.authenticate('local',{
-        successRedirect: '/edit',
-        failureRedirect: '/'
-      })
-    }
-  })
+  if (req.user){
+    console.log(req.user)
+    User.findOne({username: req.body.username}, function(err, user){
+      console.log("The user is: "+user.admin);
+      if (user.admin === "true"){
+        res.redirect('/admin');
+        // admin = true;
+        // passport.authenticate('local',{
+        //   successRedirect: '/admin',
+        //   failureRedirect: '/'
+        // })
+      } else{
+        res.redirect('/edit');
+        // passport.authenticate('local',{
+        //   successRedirect: '/edit',
+        //   failureRedirect: '/'
+        // })
+      }
+    })
+  }
+
 })
 
 
