b43b30087e2e419b13f9f80d4c7f7834f42b8c08
Naren Dhyani <31188643+dhyani15@users.noreply.github.com>
2022-09-30 06:16:04 -0400

    bugfix

--- public/dashboard_helpers.js
+++ public/dashboard_helpers.js
@@ -1,6 +1,8 @@
 // load the users in the table
 async function loadUsers() {
-    const users = await fetch("/users").then(async (res) => await res.json());
+    let res = await fetch('/users');
+    const users = await res.json();
+    // const users = await fetch("/users").then(async (res) => await res.json());
     const table = document.getElementById("users");
     console.log(users);
     users.forEach((user, index) => {
