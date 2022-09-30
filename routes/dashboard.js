6786d742a3fc728b03b46338a72ba6874154cea0
Naren Dhyani <31188643+dhyani15@users.noreply.github.com>
2022-09-30 07:02:20 -0400

    fix

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 router.get('/', function(req, res) {
-    if (req.isAuthenticated()){
+    if (req.isAuthenticated() && req.user.admin === true){
         console.log('Sucess')
         res.sendFile(path.join(__dirname,'..','/public/dashboard.html'));
     } else {
